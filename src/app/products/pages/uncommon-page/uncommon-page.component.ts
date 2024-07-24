6601eee217d47987df03e1c53232ca30c10af39d
Victor Rodriguez Garcia <vrodriguez@opentrends.net>
2024-07-24 17:32:06 +0200

    pipes por defecto final

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
+  styles: `
+  .p-fieldset{
+    height : 100%;
+  }
+  `,
 })
 export class UncommonPageComponent {
   //i18nSelect
@@ -22,4 +28,33 @@ export class UncommonPageComponent {
       this.gender = 'male';
     }
   }
+
+  //i18nPlural
+  clients = ['Fernando', 'Carlos', 'Juan', 'Miguel', 'Ana', 'Luisa', 'Carmen'];
+  clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos 1 cliente esperando.',
+    other: 'tenemos # clientes esperando.',
+  };
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  //KeyValue Pipe
+  person = {
+    name: 'Víctor',
+    lastname: 'Rodríguez García',
+    age: 45,
+    address: 'Anxo Senra Fernandez, 15',
+  };
+
+  //Async Pipe
+  myObservableTimer = interval(1000);
+
+  promiseValue = new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos info en la promesa.');
+    }, 4000);
+  });
 }
